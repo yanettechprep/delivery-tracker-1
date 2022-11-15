b14e92c232518cc036e1016e9a5d915743b2bfec
Jelani Woods <jelani@firstdraft.com>
2022-11-15 12:21:47 -0600

    Test improvements

--- spec/features/02_received_spec.rb
+++ spec/features/02_received_spec.rb
@@ -187,6 +187,44 @@ describe "The text of the expected arrival date" do
   end
 end
 
+describe "The text of the expected arrival date" do
+  it "is black when the date is less than 3 days ago", points: 1, js: true do
+    visit("/user_sign_in")
+    user_jacob = User.new
+    user_jacob.email = "jacob_#{rand(100)}@example.com"
+    user_jacob.password = "password"
+    user_jacob.save
+
+    visit "/user_sign_in"
+
+    within(:css, "form") do
+      fill_in "Email", with: user_jacob.email
+      fill_in "Password", with: user_jacob.password
+      find("button", :text => /Sign in/i ).click
+    end
+
+    visit("/")
+
+    date = 1.day.ago.to_date
+    formatted_date = date.strftime("%m/%d/%Y")
+
+    within(:css, "form") do
+      fill_in "Description", with: "New phone"
+      fill_in "Supposed to arrive on", with: formatted_date
+      fill_in "Details", with: "This package is important!"
+      find("button", :text => /Log delivery/ ).click
+    end
+
+    formatted_date.gsub!("/", "\/")
+    date_pattern = "/Supposed to arrive on\\s*#{date}/"
+
+    visit("/")
+    element_with_late_date = find_parent_element_from_text(date_pattern)
+
+    expect(element_with_late_date).to have_color("black")
+  end
+end
+
 # Must send String containing Regex
 # returns first matching element (Capybara Node)
 def find_parent_element_from_text(text)
