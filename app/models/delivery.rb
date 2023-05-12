7f7186be77bbbb6ed2de37cbbc5a02e62fda6175
YanetRoman <117314595+yanettechprep@users.noreply.github.com>
2023-05-12 17:06:25 +0000

    added assoc. for user/deliveries

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: deliveries
+#
+#  id          :integer          not null, primary key
+#  arrive_on   :date
+#  description :string
+#  details     :string
+#  status      :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
 class Delivery < ApplicationRecord
+  belongs_to(:user, { :required => true, :class_name => "User", :foreign_key => "user_id" })
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
   validates :email, :uniqueness => { :case_sensitive => false }
   validates :email, :presence => true
   has_secure_password
+  has_many(:todos, { :class_name => "Delivery", :foreign_key => "user_id", :dependent => :destroy })
 end
